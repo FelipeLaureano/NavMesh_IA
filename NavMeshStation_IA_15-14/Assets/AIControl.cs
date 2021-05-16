2ea32559564a41634aa67c6952583a87561cbcc3
FelipeLaureano <felipe.laureano@hotmail.com>
2021-05-16 16:42:57 -0300

    Atualização

---
+++ NavMeshStation_IA_15-14/Assets/AIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AIControl : MonoBehaviour
+{
+    public UnityEngine.AI.NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();//pegar NavMesh
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ NavMeshStation_IA_15-14/Assets/AgentManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Pega clique do mouse  e levar para posição clicada
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if(Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach(GameObject a in agents)
+                {
+                    a.GetComponent<AIControl>().agent.SetDestination(hit.point);
+                }
+            }
+        }
+    }
+}
